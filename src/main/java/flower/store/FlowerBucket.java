6ab5bab3199dd75a80235912d0db43147ffff0ea
Shchur Oleksandr <Olexander.Shchur@gmail.com>
2022-10-23 22:34:31 +0300

    Added some parts of FlowerBucket, FlowerPack and Store

--- src/main/java/flower/store/FlowerBucket.java
+++ src/main/java/flower/store/FlowerBucket.java
@@ -1,5 +1,18 @@
 package flower.store;
 
-public class FlowerBucket {
+import java.util.ArrayList;
 
+public class FlowerBucket {
+    private ArrayList<FlowerPack> flowerPacks = new ArrayList<>();
+    public void add(FlowerPack flowerPack){
+        flowerPacks.add(flowerPack);
+    }
+
+    public double getPrice(){
+        int price = 0;
+        for (FlowerPack flowerPack: flowerPacks){
+            price += flowerPack.get_price();
+        }
+        return price;
+    }
 }
--- src/main/java/flower/store/FlowerPack.java
+++ src/main/java/flower/store/FlowerPack.java
@@ -3,4 +3,12 @@ package flower.store;
 public class FlowerPack {
     private Flower flower;
     private int quantity;
+    public FlowerPack(Flower flower, int quantity){
+        this.flower = flower;
+        this.quantity = quantity;
+    }
+
+    public double get_price(){
+        return quantity*flower.getPrice();
+    }
 }
