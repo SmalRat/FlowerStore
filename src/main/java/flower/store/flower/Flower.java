359590603c907e4c00870d3311d1368a122d3ebc
Shchur Oleksandr <Olexander.Shchur@gmail.com>
2022-10-27 19:25:46 +0300

    Some fixes

--- src/main/java/flower/store/flower/Flower.java
+++ src/main/java/flower/store/flower/Flower.java
@@ -7,15 +7,34 @@ import lombok.Setter;
 
 @Setter
 public class Flower {
+    /**
+     * Flower class
+     */
+
     @Getter
     private double sepalLength;
+    /**
+     * Sepal length
+     */
     private FlowerColor color;
+
+    /**
+     * Color of the flower
+     */
     @Getter
     private double price;
+    /**
+     * The price of flower
+     */
     @Getter
     private FlowerType flowerType;
-
+    /**
+     * Type of flower
+     */
     public final String getColor() {
+        /**
+         * returns color (string)
+         */
         return color.toString();
     }
 }
