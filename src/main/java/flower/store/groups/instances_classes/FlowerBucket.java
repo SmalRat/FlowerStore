c3d90b18c2346b1d472aefb6f19bcffddeb16909
Shchur Oleksandr <Olexander.Shchur@gmail.com>
2022-10-27 19:57:16 +0300

    Some fixes again

--- src/main/java/flower/store/groups/instances_classes/FlowerBucket.java
+++ src/main/java/flower/store/groups/instances_classes/FlowerBucket.java
@@ -6,22 +6,17 @@ import java.util.List;
 
 public final class FlowerBucket {
     private FlowerBucketSpecs specs = new FlowerBucketSpecs();
-
     private List<FlowerPack> flowerPacks = new ArrayList<>();
-
     public void add(final FlowerPack flowerPack) {
         flowerPacks.add(flowerPack);
         specs.add(flowerPack);
     }
-
-    public ArrayList<FlowerPack> getFlowerPacks() {
+    /*public ArrayList<FlowerPack> getFlowerPacks() {
         return new ArrayList<FlowerPack>(flowerPacks);
-    }
-
+    }*/
     public boolean matches(final FlowerBucketSpecs otherSpecs) {
         return (this.specs.equal(otherSpecs));
     }
-
     public double getPrice() {
         return specs.getPrice();
     }
